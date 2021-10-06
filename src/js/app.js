8debe35474576f621c962f4adabb958f5d428586
Porto <rodrigoandradeporto@hotmail.com>
2021-10-05 23:21:22 -0300

    style: remove unused lines

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,10 @@
-/*
-  MQTT.Cool - https://mqtt.cool
-
-  MQTT Chat Demo
-
-  Copyright (c) Lightstreamer Srl
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
 'use strict';
 
 const USERS_LIST_TOPIC_PREFIX = 'users';
 const USERS_LIST_TOPIC = USERS_LIST_TOPIC_PREFIX + '/#';
 const CHAT_ROOM_TOPIC = 'chat';
 
-// Target MQTT.Cool address. Change it with if required.
+// Target MQTT.Cool address.
 const MQTT_COOL_URL = 'http://localhost:8080';
 
 // Default MQTT broker to connect to.
